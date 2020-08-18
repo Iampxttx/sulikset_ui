ddd2a754cb6bce8c07a485066c9508c6ebe23db1
Phuc Huynh <Putte.huynh@gmail.com>
2020-08-18 11:41:19 +0300

    20200818

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './Components/Layout/Header';
+
+function App() {
+
+  const initJobs = [
+    {
+      "id": 1,
+      "tyotehtava": "Lastenhoitaja"
+    },
+    {
+      "id": 2,
+      "tyotehtava": "Lakaisukoneen kuljettaja"
+    },
+    {
+      "id": 3,
+      "tyotehtava": "Siwan myyjä"
+    }
+  ]
+  const [jobs, setJobs] = useState(initJobs);
+  
+  const rows = () => jobs.map(job => {
+    return <p>{job.tyotehtava}</p>
+  })
+  return (
+    <div className="App">
+      <Header />
+      {rows()}
+    </div>
+  );
+}
+
+export default App;
