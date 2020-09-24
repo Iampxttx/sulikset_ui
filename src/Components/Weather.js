6f851311dec21f023f0b02c7ac7f68122f061dca
Phuc Huynh <phuc.huynh@ict.opetus.local>
2020-09-24 10:36:47 +0300

    24092020

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { VictoryChart, VictoryLine, VictoryBar } from 'victory'
 
 function Weather() {
@@ -7,63 +7,45 @@ function Weather() {
     const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
 
     const initWeather = [];
-    const [weather, setWeather] = use(initWeather);
+    const [weather, setWeather] = useState(initWeather);
 
     fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==&amount=50')
         .then(response => response.json())
         .then(json => setWeather([...json]));
-    
-let humtepkey = 1;
-let charTempData = [];
-let charhumData = [];
-const rows = () => weather.slice(0, 24).reverse().map(temphum => {
-    const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-'[1] + '.' + temphum.PublishedAt.split('T'[0].split('-')[0];
-    const measurementDate = temphum.PublishedAt.split('T')[1].split(':')[0] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1];
-    chartTempData.push({ x: String(measurementTime), y: parseInt(temphum.Temp) });
-    chartHumData.push({ experiment: String(measurementTime), actual: parseInt(temphum.Hum), label: String(temphum.Hum.split('.')[0])+"%" });
-    return <div key={humptemkey++}><b>Pvm: </b>{measurementDate}, <b>klo:</b> {measurementTime} <b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}% <b>Lämpötila:</b> {temphum.Temp}.split('.')[0]}°C</div>
-})
 
-const showTempature = charTempData;
-const ShowHumidity = chartHumData;
+    let humptemkey = 1;
+    let charttemp = [];
+    let charthum = [];
+    weather.slice(0, 24).reverse().map(temphum => {
+        const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0];
+        const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1];
+        charttemp.push({ x: String(measurementTime), y: parseInt(temphum.Temp) });
+        charthum.push({ Päivämäärä: String(measurementTime), Ilmankosteus: parseInt(temphum.Hum), label: String(temphum.Hum.split('.')[0]) + "%" });
+        return <div key={humptemkey++}><b>Pvm: </b>{measurementDate}, <b>klo:</b> {measurementTime} <b>Ilmankosteus:</b> {temphum.Hum.split('.')[0]}% <b>Lämpötila:</b> {temphum.Temp}.split('.')[0]}°C</div>
+    })
 
+    const temperatureData = charttemp;
+    const humidityData = charthum;
 
-   
-   
-    const temperatureData = [
-        { experiment: "1.1.", actual: -10 },
-        { experiment: "2.1.", actual: 15 },
-        { experiment: "3.1.", actual: 0 },
-        { experiment: "4.1.", actual: 1 },
-        { experiment: "5.1.", actual: 2 },
-        { experiment: "6.1.", actual: 3 }
-    ]
-
-    const humidityData = [
-        { Päivämäärä: "1.1.", Ilmankosteus: 61 },
-        { Päivämäärä: "2.1.", Ilmankosteus: 64 },
-        { Päivämäärä: "3.1", Ilmankosteus: 67 },
-        { Päivämäärä: "4.1.", Ilmankosteus: 70 },
-    ]
     return (
         <div>
+            
             <VictoryChart
                 domainPadding={{ x: 20, y: 10 }}
-                width={1000}
-                height={250}>
+                width={1300}
+                height={350}>
                 <VictoryLine
                     data={temperatureData}
                     style={{
                         data:
                             { stroke: "green", strokeWidth: 1 }
                     }}
-                    x="expreriment"
-                    y="actual" />
+                    />
 
             </VictoryChart>
             <VictoryChart
-                width={1000}
-                height={250}
+                width={1300}
+                height={350}
                 domainPadding={{ x: 150, y: 10 }}>
                 <VictoryChart>
                     <VictoryBar
